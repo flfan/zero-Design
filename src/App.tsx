5986fb2f88e9aced7bde93cbe15ba6845ac41deb
flfan <1490459938@qq.com>
2020-06-21 18:19:57 +0800

    init button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button, {ButtonType} from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button>Hello</Button>
+        <Button disabled>Hello</Button>
+        <Button disabled btnType={ButtonType.Link} href={'https://www.baidu.com'}>baidulink</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
