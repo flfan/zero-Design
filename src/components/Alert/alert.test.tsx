633c71991218201ab1c5fc4abe312e797b557e7b
flfan <1490459938@qq.com>
2020-06-22 14:13:31 +0800

    add unit test to component alert and button

---
+++ src/components/Alert/alert.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import Alert from './alert'
+
+describe('test Alert component',() => {
+  it('render the correct Alert box', () => {
+    const warpper = render(<Alert title={'hello'} alertType={'success'}></Alert>)
+    const element = warpper.getByText('hello')
+    expect(element).toBeInTheDocument()
+    // expect(element?.getAttribute('alertType')).toEqual('success')
+  })
+})
