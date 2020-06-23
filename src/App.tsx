380da59222db41148b92348781d53ac18707a08c
flfan <1490459938@qq.com>
2020-06-23 17:33:23 +0800

    add animation to alert component

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,10 @@ import Transition from './components/Transition/transition'
 
 // font awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas, faCoffee } from '@fortawesome/free-solid-svg-icons'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-library.add(fas)
+// library.add(fas)
 
 const element = (
   <>
--- src/components/Alert/alert.tsx
+++ src/components/Alert/alert.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import classNames from 'classnames'
-
+import Icon from '../Icon/icon'
+import Transition from '../Transition/transition'
 type AlertType = 'success' | 'default' | 'danger' | 'warning'
 
 interface AlertProps {
@@ -17,17 +18,21 @@ const Alert: React.FC<AlertProps> = (props) => {
   const classes = classNames('alert-container', {
     [`alert-${alertType}`]: alertType
   })
-  if (!close) {
+
     return (
+      <Transition 
+        in={!close}
+        timeout={500}
+        animation={'zoom-in-right'}
+      >
       <div className={classes}>
         <div className={'alert-title'}>{title}</div>
         {showDetail ? <div className={'alert-detail'}>{detail}</div> : null}
-        {showClose ? <div onClick={e => {setClose(true);e.nativeEvent.stopImmediatePropagation()}} className={'alert-close'}>close</div> : null}
+        {showClose ? <div onClick={e => {setClose(true);e.nativeEvent.stopImmediatePropagation()}} className={'alert-close'}><Icon size={'2x'} icon="times"/></div> : null}
       </div>
+      </Transition>
     )
-  } else {
-    return null
-  }
+  
   
 }
 
