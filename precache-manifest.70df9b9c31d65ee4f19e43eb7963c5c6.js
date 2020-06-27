baf85f3639d8df0796782f22827e75cf3c1ba9b7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-27 09:20:10 +0000

    Deploy flfan/zero-design to github.com/flfan/zero-design.git:gh-pages

---
+++ precache-manifest.70df9b9c31d65ee4f19e43eb7963c5c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0fe5d349ffdc481ed50c7d63bfbb52",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.62fa27ee16c5a63f2631.bundle.js"
+  },
+  {
+    "url": "runtime~main.62fa27ee16c5a63f2631.bundle.js"
+  },
+  {
+    "revision": "762b257c601d5f5fc544",
+    "url": "static/css/main.df3f6a55.chunk.css"
+  },
+  {
+    "revision": "1c538923401f6e588934",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.62fa27ee16c5a63f2631.bundle.js"
+  },
+  {
+    "url": "vendors~main.62fa27ee16c5a63f2631.bundle.js.LICENSE.txt"
+  }
+]);
