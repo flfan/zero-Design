8843cebb10f721746c07ed13619bd11fee497521
flfan <1490459938@qq.com>
2020-06-27 16:56:19 +0800

    traivs test

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -3,18 +3,18 @@ import { render, RenderResult, fireEvent, wait } from '@testing-library/react'
 import Menu, {MenuProps} from './menu'
 import MenuItem from './menuItem'
 import SubMenu from './subMenu'
-// jest.mock('../Icon/icon', () => {
-//   return () => {
-//     return <i className="fa" />
-//   }
-// })
-// jest.mock('react-transition-group', () => {
-//   return {
-//     CSSTransition: (props: any) => {
-//       return props.children
-//     }
-//   }
-// })
+jest.mock('../Icon/icon', () => {
+  return () => {
+    return <i className="fa" />
+  }
+})
+jest.mock('react-transition-group', () => {
+  return {
+    CSSTransition: (props: any) => {
+      return props.children
+    }
+  }
+})
 const testProps: MenuProps = {
   defaultIndex: '0',
   onSelect: jest.fn(),
@@ -52,10 +52,10 @@ const generateMenu = (props: MenuProps) => {
 }
 const createStyleFile = () => {
   const cssFile: string = `
-    .zero-submenu {
+    .viking-submenu {
       display: none;
     }
-    .zero-submenu.menu-opened {
+    .viking-submenu.menu-opened {
       display:block;
     }
   `
@@ -75,7 +75,7 @@ describe('test Menu and MenuItem component in default(horizontal) mode', () => {
   })
   it('should render correct Menu and MenuItem based on default props', () => {
     expect(menuElement).toBeInTheDocument()
-    expect(menuElement).toHaveClass('zero-menu test')
+    expect(menuElement).toHaveClass('viking-menu test')
     expect(menuElement.querySelectorAll(':scope > li').length).toEqual(5)
     expect(activeElement).toHaveClass('menu-item is-active')
     expect(disabledElement).toHaveClass('menu-item is-disabled')
